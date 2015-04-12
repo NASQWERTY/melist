808e8a3e1b13b86430083dcae0b7a3e5c1ff721b
Nelson Secchi <nas.qwerty@gmail.com>
2015-04-12 07:13:33 -0300

    Revert "Authentication changed to use a holder"
    This reverts commit c261c7376a014149da6aead89cac0d3d0507da59.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,7 +3,7 @@ import org.springframework.beans.factory.config.BeanDefinition
 
 // Place your Spring DSL code here
 beans = {
-    meliHolder(MeliHolder) { beanDefinition ->
+    meliObject(Meli) { beanDefinition ->
         beanDefinition.constructorArgs = [grailsApplication.metadata['application.meli.appId'] as Long,
                                           grailsApplication.metadata['application.meli.secretKey']]
     }
